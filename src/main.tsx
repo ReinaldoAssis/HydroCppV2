ce45dc87fc6f3b807487d9fe69f1bdc71dfb2a2a
Reinaldo Assis <randroi75@gmail.com>
2022-12-17 20:12:44 -0300

    feat: random size

--- src/main.tsx
+++ src/main.tsx
@@ -32,8 +32,35 @@ const actions: SpotlightAction[] = [
   },
   {
     title: "Run random simulation",
-    description: "Random protein sequence",
+    description: "Random protein sequence | alias: 'rsim'",
     onTrigger: () => {
+      globalState.random_sim?.();
+    },
+
+    // icon: <IconFileText size={18} />,
+  },
+  {
+    title: "Toggle sequence",
+    description: "Show/hide protein sequence | alias: 'seq'",
+    onTrigger: () => {
+      globalState.show_sequence?.();
+    },
+
+    // icon: <IconFileText size={18} />,
+  },
+  {
+    title: "Randomize random size",
+    description: "Set random size as a random number | alias: 'rnd'",
+    onTrigger: () => {
+      let rnd = Math.floor(Math.random() * 20);
+      if (rnd < 10) {
+        let a = rnd * 2 + 2;
+        let b = rnd * (rnd + 2);
+        rnd = Math.sqrt(a * a + b * b);
+      } else {
+        rnd = Math.floor(Math.random() * 200);
+      }
+      globalState.rndSize = rnd;
       globalState.random_sim?.();
     },
 
@@ -43,7 +70,11 @@ const actions: SpotlightAction[] = [
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <SpotlightProvider shortcut={["mod + P", "mod + K", "/"]} actions={actions}>
+    <SpotlightProvider
+      highlightQuery
+      shortcut={["mod + P", "mod + K", "/"]}
+      actions={actions}
+    >
       <NotificationsProvider>
         <App />
       </NotificationsProvider>
